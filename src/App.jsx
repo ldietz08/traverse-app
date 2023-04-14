5a863dbdefaab3ba4af93a0d5f9a9dbc342d0468
Lakpa Dietz <ldietz086@gmail.com>
2023-04-14 16:31:18 -0700

    Created bulletin posts component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/header/Header";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import LandingPage from "./pages/landing-page/LandingPage";
-import Hero from "./pages/hero/Hero";
+import Explore from "./pages/explore/Explore";
 import Auth from "./pages/login/Login";
 import Bulletin from "./pages/bulletin/Bulletin";
 import Footer from "./components/footer/Footer";
@@ -20,7 +20,7 @@ export default function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
-            <Route path="hero" element={<Hero />} />
+            <Route path="explore" element={<Explore />} />
             <Route path="bulletin" element={<Bulletin />} />
           </Routes>
           <Footer />
